69e0dac727385a8f24453e25e4f8917ab80e196d
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-16 16:25:33 +0200

    get market summaries test passsing

--- lib/bittrex_ex.ex
+++ lib/bittrex_ex.ex
@@ -2,7 +2,12 @@ defmodule BittrexEx do
   use HTTPoison.Base
 
   def get_market_summaries do
-    get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
-    |> Poison.Parser.parse!
+      with {:ok, response} <- get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
+      do
+        body = Poison.Parser.parse!(response.body)
+        body["result"]
+      else {:error, reason} -> {:error, reason}
+    end
   end
+
 end
--- test/bittrex_ex_test.exs
+++ test/bittrex_ex_test.exs
@@ -3,6 +3,6 @@ defmodule BittrexExTest do
   doctest BittrexEx
 
   test "get market summaries" do
-    assert [%{"last" => 0} | _ ] = BittrexEx.get_market_summaries
+    assert [%{"Last" => _} | _ ] = BittrexEx.get_market_summaries
   end
 end
