a6c64c87f6f163aabd26a31c5af97e5d8ce5f0b8
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-03-22 22:18:03 +0200

    add result example to get_market_summaries fn

--- lib/bittrex_ex.ex
+++ lib/bittrex_ex.ex
@@ -1,6 +1,29 @@
 defmodule Bittrex do
   use HTTPoison.Base
 
+  @doc """
+  Returns a list of "market summaries" for last 24 hours
+
+  Example "market summary":
+  ```
+  {
+			"MarketName" : "BTC-888",
+			"High" : 0.00000919,
+			"Low" : 0.00000820,
+			"Volume" : 74339.61396015,
+			"Last" : 0.00000820,
+			"BaseVolume" : 0.64966963,
+			"TimeStamp" : "2014-07-09T07:19:30.15",
+			"Bid" : 0.00000820,
+			"Ask" : 0.00000831,
+			"OpenBuyOrders" : 15,
+			"OpenSellOrders" : 15,
+			"PrevDay" : 0.00000821,
+			"Created" : "2014-03-20T06:00:00",
+			"DisplayMarketName" : null
+		}
+  ```
+  """
   def get_market_summaries do
       with {:ok, response} <- get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
       do
